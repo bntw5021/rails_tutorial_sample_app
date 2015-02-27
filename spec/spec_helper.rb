49472fe835e73352d361d083f2893cbd7d7d8404
bntw5021 <watanabe1205@gmail.com>
2015-02-27 23:05:20 +0900

    after '3.2.2 add page'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -77,6 +77,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = :random
+  config.include Capybara::DSL
+#  config.include Rails.application.routes.url_helpers
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
